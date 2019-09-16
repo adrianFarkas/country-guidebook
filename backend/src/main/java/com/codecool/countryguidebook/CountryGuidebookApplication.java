282e146071935bbfac1f2da62f02e1fc43773cda
adrianFarkas <farkasadrian1996@gmail.com>
2019-09-16 11:35:19 +0200

    setup backend

---
+++ backend/src/main/java/com/codecool/countryguidebook/CountryGuidebookApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.countryguidebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryGuidebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryGuidebookApplication.class, args);
+    }
+
+}
