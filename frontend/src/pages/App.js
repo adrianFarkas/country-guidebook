e63af3cd24c7e90b2ce026bc6907777e3cf7005a
adrianFarkas <farkasadrian1996@gmail.com>
2019-09-17 14:10:27 +0200

    created basic navbar

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from "../components/Header";
+import EuMap from "../components/EuMap";
 
 function App() {
   return (
     <div className="App">
-
+        <Header text="Country Guide"/>
+        <EuMap />
     </div>
   );
 }
