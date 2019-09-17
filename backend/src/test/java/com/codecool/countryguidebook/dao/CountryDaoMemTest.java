44e62e6e1c2dd7735ab41ca6ab13546f85f96e36
GaborPap <ppgabor@gmail.com>
2019-09-17 23:05:56 +0200

    Refactoring recurrent code.

--- backend/src/test/java/com/codecool/countryguidebook/dao/CountryDaoMemTest.java
+++ backend/src/test/java/com/codecool/countryguidebook/dao/CountryDaoMemTest.java
@@ -6,6 +6,7 @@ import com.codecool.countryguidebook.model.Language;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -16,36 +17,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CountryDaoMemTest {
 
-
+private CountryDaoMem countryDaoMem = new CountryDaoMem();
     private List<Language> hun = Collections.singletonList(Language.HUNGARIAN);
     private List<Language> ger = Collections.singletonList(Language.GERMAN);
     private List<Language> gerHun = Arrays.asList(Language.HUNGARIAN, Language.GERMAN);
-    private List<Language> gerHIta = Arrays.asList(Language.ITALIAN, Language.GERMAN);
-    private List<Language> fra = Collections.singletonList(Language.FRENCH);
     private List<Language> ita = Collections.singletonList(Language.ITALIAN);
 
-    @Before
+    @BeforeEach
     public void init(){
+        countryDaoMem.clear();
+        Country c1 = new Country("Hungary", CountryCode.HUN,456, "EUR", hun);
+        countryDaoMem.add(c1);
+        Country c2 = new Country("Germany", CountryCode.DEU, 1200, "EUR", gerHun);
+        countryDaoMem.add(c2);
 
     }
 
     @Test
     public void addCountryToDao(){
-        CountryDaoMem countryDaoMem = new CountryDaoMem();
-        Country c1 = new Country("Hungary", CountryCode.HUN,456, "EUR", hun);
-        countryDaoMem.add(c1);
-        Country c2 = new Country("Germany", CountryCode.DEU, 1200, "EUR", gerHun);
-        countryDaoMem.add(c2);
         Assert.assertEquals(2, countryDaoMem.getCountries().size());
     }
 
     @Test
     public void filterCountriesByPopulationWithRange(){
-        CountryDaoMem countryDaoMem = new CountryDaoMem();
-        Country c1 = new Country("Hungary", CountryCode.HUN,456, "EUR", hun);
-        countryDaoMem.add(c1);
-        Country c2 = new Country("Germany", CountryCode.DEU, 1200, "EUR", gerHun);
-        countryDaoMem.add(c2);
         Assert.assertEquals(1, countryDaoMem.findByPopulation(countryDaoMem.getCountries(),100,600).size());
         Assert.assertEquals(1, countryDaoMem.findByPopulation(countryDaoMem.getCountries(),1000,2000).size());
         Assert.assertEquals(2, countryDaoMem.findByPopulation(countryDaoMem.getCountries(),100,2111).size());
@@ -53,11 +47,7 @@ class CountryDaoMemTest {
 
     @Test
     public void filterCountriesByLanguages(){
-        CountryDaoMem countryDaoMem = new CountryDaoMem();
-        Country c1 = new Country("Hungary", CountryCode.HUN,456, "EUR", hun);
-        countryDaoMem.add(c1);
-        Country c2 = new Country("Germany", CountryCode.DEU, 1200, "EUR", gerHun);
-        countryDaoMem.add(c2);
+
         Assert.assertEquals(2, countryDaoMem.findByLanguage(countryDaoMem.getCountries(),hun).size());
         Assert.assertEquals(1, countryDaoMem.findByLanguage(countryDaoMem.getCountries(),ger).size());
         Assert.assertEquals(0, countryDaoMem.findByLanguage(countryDaoMem.getCountries(),ita).size());
