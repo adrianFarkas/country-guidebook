d2fa726db5f940e151c3ba8d945fcbd752b53beb
adrianFarkas <farkasadrian1996@gmail.com>
2019-09-19 17:50:09 +0200

    modified filter titles

--- frontend/src/components/filter/FilterDropdown.js
+++ frontend/src/components/filter/FilterDropdown.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class FilterDropdown extends Component {
 
-    capitalizeIfLanguage(letter) {
+    capitalizeIfNotISOCode(letter) {
         if (letter.length > 3) {
             const lowercase = letter.toLowerCase();
             return lowercase.charAt(0).toUpperCase() + lowercase.slice(1)
@@ -14,11 +14,11 @@ class FilterDropdown extends Component {
         const { category, data, name } = this.props;
         return (
             <div className="selection">
-                <label className="label">Sort by {category}</label>
+                <label className="label">{this.capitalizeIfNotISOCode(category)}</label>
                 <select className="options" name={name}>
                     <option value="default">Default</option>
                     {data.map((option, index) =>
-                        <option key={index} value={option}>{this.capitalizeIfLanguage(option)}</option>)}
+                        <option key={index} value={option}>{this.capitalizeIfNotISOCode(option)}</option>)}
                 </select>
             </div>
         );
