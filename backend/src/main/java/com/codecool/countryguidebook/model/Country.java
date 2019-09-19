af3fbf3dfb0050c56fdb0ee15d0965c94d76bd71
BihackerViktor <bihacker.viktor@gmail.com>
2019-09-19 09:38:45 +0200

    created classes for countries

--- backend/src/main/java/com/codecool/countryguidebook/model/Country.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/Country.java
@@ -39,36 +39,36 @@ public class Country {
 
         Country country = new Country();
 
-        JSONArray currenci = (JSONArray) json.get("currencies");
-        JSONArray langu = (JSONArray) json.get("languages");
+        JSONArray currencies = (JSONArray) json.get("currencies");
+        JSONArray languages = (JSONArray) json.get("languages");
         country.currencies = new ArrayList<>();
         country.languages = new ArrayList<>();
         country.timezones = new ArrayList<>();
         country.callingCodes = new ArrayList<>();
 
 
-        for (int i = 0; i < currenci.length(); i++
+        for (int i = 0; i < currencies.length(); i++
         ) {
 
-            country.currencies.add(currenci.getJSONObject(i).get("name").toString());
+            country.currencies.add(currencies.getJSONObject(i).get("name").toString());
         }
 
-        for (int i = 0; i < langu.length(); i++) {
-            country.languages.add(Language.valueOf(langu.getJSONObject(i).get("name").toString().toUpperCase().split("\\s+")[0]));
+        for (int i = 0; i < languages.length(); i++) {
+            country.languages.add(Language.valueOf(languages.getJSONObject(i).get("name").toString().toUpperCase().split("\\s+")[0]));
         }
         country.name = json.get("name").toString();
         country.alpha3Code = CountryCode.valueOf(json.get("alpha3Code").toString());
         country.area = json.get("area").toString();
-        JSONArray callingC = (JSONArray) json.get("callingCodes");
-        for (int i = 0; i < callingC.length(); i++) {
+        JSONArray callingCodes = (JSONArray) json.get("callingCodes");
+        for (int i = 0; i < callingCodes.length(); i++) {
 
-            country.callingCodes.add(callingC.get(0).toString());
+            country.callingCodes.add(callingCodes.get(0).toString());
 
 
         }
-        JSONArray tzones = (JSONArray) json.get("timezones");
-        for (int i = 0; i < tzones.length(); i++) {
-            country.timezones.add(tzones.get(0).toString());
+        JSONArray timezones = (JSONArray) json.get("timezones");
+        for (int i = 0; i < timezones.length(); i++) {
+            country.timezones.add(timezones.get(0).toString());
         }
         country.population = Long.parseLong(json.get("population").toString());
         country.capital = json.get("capital").toString();
---
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Population.java
@@ -0,0 +1,4 @@
+package com.codecool.countryguidebook.model.countrybuilder;
+
+public class Population {
+}
