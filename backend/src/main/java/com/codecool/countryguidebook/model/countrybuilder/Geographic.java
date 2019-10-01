6d6bb5968c07664714b23aefd62b473ba6726eb0
BihackerViktor <bihacker.viktor@gmail.com>
2019-10-01 14:24:31 +0200

    fix for geographic and units

--- backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Geographic.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Geographic.java
@@ -5,10 +5,7 @@ import com.codecool.countryguidebook.model.CountryCode;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Component
@@ -20,35 +17,27 @@ import java.util.List;
 @Builder
 @Entity
 public class Geographic {
+
     @Id
     @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
     private CountryCode alpha3Code;
+
     private String capital;
     private String subRegion;
     private long population;
     private int area;
+
+    @Singular
+    @ElementCollection
     private List<String> timeZones;
+
+    @Singular
+    @ElementCollection
     private List<String> callingCodes;
 
     @OneToOne
     private Country country;
-
-/*    public Geographic createGeographic(JSONObject json) throws JSONException {
-        this.timezones = new ArrayList<>();
-        this.callingCodes = new ArrayList<>();
-        this.alpha3Code = CountryCode.valueOf(json.get("alpha3Code").toString());
-        this.area = (int) Double.parseDouble(json.get("area").toString());
-        JSONArray callingCodes = (JSONArray) json.get("callingCodes");
-        for (int i = 0; i < callingCodes.length(); i++) {
-            this.callingCodes.add(callingCodes.get(0).toString());
-        }
-        JSONArray timezones = (JSONArray) json.get("timezones");
-        for (int i = 0; i < timezones.length(); i++) {
-            this.timezones.add(timezones.get(0).toString());
-        }
-        this.population = Long.parseLong(json.get("population").toString());
-        this.capital = json.get("capital").toString();
-        this.subregion = json.get("subregion").toString();
-        return this;
-    }*/
 }
--- backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Units.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Units.java
@@ -5,10 +5,7 @@ import com.codecool.countryguidebook.model.Language;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Component
@@ -22,23 +19,14 @@ import java.util.List;
 public class Units {
     @Id
     @GeneratedValue
+    private Long id;
+    @Singular
+    @ElementCollection
     private List<String> currencies;
+    @Singular
+    @ElementCollection
     private List<Language> languages;
 
     @OneToOne
     private Country country;
-
-    /*public Units createUnits(JSONObject json) throws JSONException {
-        JSONArray currencies = (JSONArray) json.get("currencies");
-        JSONArray languages = (JSONArray) json.get("languages");
-        this.currencies = new ArrayList<>();
-        this.languages = new ArrayList<>();
-        for (int i = 0; i < currencies.length(); i++) {
-            this.currencies.add(currencies.getJSONObject(i).get("name").toString());
-        }
-        for (int i = 0; i < languages.length(); i++) {
-            this.languages.add(Language.valueOf(languages.getJSONObject(i).get("name").toString().toUpperCase().split("\\s+")[0]));
-        }
-        return this;
-    }*/
 }
