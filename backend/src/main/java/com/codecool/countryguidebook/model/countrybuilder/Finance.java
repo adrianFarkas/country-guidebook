52e36b67fa3050219312ec77d8051b30083fec13
GaborPap <ppgabor@gmail.com>
2019-10-01 14:06:06 +0200

    Added annotations to class

--- backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Finance.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Finance.java
@@ -1,5 +1,19 @@
 package com.codecool.countryguidebook.model.countrybuilder;
 
+
+import com.codecool.countryguidebook.model.Country;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Component
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Finance {
     private static Finance finance;
     private int stateDebtMillionEuro;
