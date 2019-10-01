a238f93b32eff5addaba25c5c97924ecf618e57c
BihackerViktor <bihacker.viktor@gmail.com>
2019-10-01 16:19:39 +0200

    addCountryToRepository_test

--- backend/src/test/java/com/codecool/countryguidebook/model/AllRepositoryTest.java
+++ backend/src/test/java/com/codecool/countryguidebook/model/AllRepositoryTest.java
@@ -3,35 +3,31 @@ package com.codecool.countryguidebook.model;
 import com.codecool.countryguidebook.repository.CountryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @DataJpaTest
 @ActiveProfiles("test")
 public class AllRepositoryTest {
 
-/*    @MockBean
-    private CountryRepository countryRepository;*/
+    @Mock
+    private CountryRepository countryRepository;
 
     @Test
-    public void addCountryToRepository () {
+    public void addCountryToRepository() {
         Country country = Country.builder()
                 .name("Hungary")
                 .build();
 
-/*        countryRepository.save(country);
-        List<Country> countries = countryRepository.findAll();
-
-        assertThat(countries).hasSize(1);*/
+        Mockito.when(countryRepository.save(Mockito.any(Country.class))).thenReturn(country);
+        countryRepository.save(country);
+        verify(countryRepository, times(1)).save(Mockito.any(Country.class));
     }
 }
