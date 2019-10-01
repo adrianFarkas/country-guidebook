2bd3e0c47c211018fb48d1c02970208c43f9ebc4
GaborPap <ppgabor@gmail.com>
2019-10-01 14:00:46 +0200

    Removed existing getters and setters.

--- backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Finance.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Finance.java
@@ -1,7 +1,6 @@
 package com.codecool.countryguidebook.model.countrybuilder;
 
 public class Finance {
-    // not in JSON
     private static Finance finance;
     private int stateDebtMillionEuro;
     private String mainLabor;
@@ -9,48 +8,5 @@ public class Finance {
     private int avarageSalaryEUR;
     private int minimumWageEUR;
 
-    public Finance() {
-        this.stateDebtMillionEuro = getStateDebtMillionEuro();
-    }
 
-
-    public int getAvarageSalaryEUR() {
-        return avarageSalaryEUR;
-    }
-
-    public void setAvarageSalaryEUR(int avarageSalaryEUR) {
-        this.avarageSalaryEUR = avarageSalaryEUR;
-    }
-
-    public int getMinimumWageEUR() {
-        return minimumWageEUR;
-    }
-
-    public void setMinimumWageEUR(int minimumWageEUR) {
-        this.minimumWageEUR = minimumWageEUR;
-    }
-
-    public int getStateDebtMillionEuro() {
-        return stateDebtMillionEuro;
-    }
-
-    public void setStateDebtMillionEuro(int stateDebtMillionEuro) {
-        this.stateDebtMillionEuro = stateDebtMillionEuro;
-    }
-
-    public String getMainLabor() {
-        return mainLabor;
-    }
-
-    public void setMainLabor(String mainLabor) {
-        this.mainLabor = mainLabor;
-    }
-
-    public int getAvarageWorkingTimePerYearInHour() {
-        return avarageWorkingTimePerYearInHour;
-    }
-
-    public void setAvarageWorkingTimePerYearInHour(int avarageWorkingTimePerYearInHour) {
-        this.avarageWorkingTimePerYearInHour = avarageWorkingTimePerYearInHour;
-    }
 }
