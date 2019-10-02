1114ff52bb953ac33cbc6a573edfea4e6b436f8f
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-02 22:15:38 +0200

    created country page header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import {Nav, Navbar} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSignInAlt, faUser} from "@fortawesome/free-solid-svg-icons";
 
-function Header() {
+function Header(props) {
     return (
-        <Navbar className="navbar" bg="dark" expand="lg" variant="dark" >
-            <Navbar.Brand href="/">Country Guide</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link href="#register"><FontAwesomeIcon icon={faUser} /> Register</Nav.Link>
-                    <Nav.Link href="#login"><FontAwesomeIcon icon={faSignInAlt} /> Login</Nav.Link>
-                </Nav>
-
-            </Navbar.Collapse>
-        </Navbar>
-    )
+        <div className="header">
+            <img src="/static/img/lanchid.jpg" alt="budapest"/>
+                <h1>Hungary</h1>
+        </div>
+    );
 }
 
 export default Header;
