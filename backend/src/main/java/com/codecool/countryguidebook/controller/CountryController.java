7194df87765a0c1dd93ce98baf8fc1742b352bf7
GaborPap <ppgabor@gmail.com>
2019-10-02 16:55:08 +0200

    Added filter with sql

--- backend/src/main/java/com/codecool/countryguidebook/controller/CountryController.java
+++ backend/src/main/java/com/codecool/countryguidebook/controller/CountryController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -32,8 +33,22 @@ public class CountryController {
 
     @PostMapping("/filter-countries")
     public List<Country> filteredCountries(@RequestBody FilterCriteria filterCriteria) {
-        List<Language> languages = Arrays.asList(Language.HUNGARIAN, Language.ITALIAN);
-        return countryRepository.getCountriesWithGivenLanguages(languages);
+       /* Map<String, Integer> population = new HashMap<>();
+        population.put("min", 100000);
+        population.put("max", 100000000);
+        filterCriteria.setPopulation(population);
+        List<Language> languages = Arrays.asList(Language.HUNGARIAN, Language.GERMAN);
+        List<Currency> currencies = Arrays.asList(Currency.EUR, Currency.CHF);
+        filterCriteria.setCurrency(currencies);
+        filterCriteria.setLanguages(languages);
+*/
+        //List<Language> languages = Arrays.asList(Language.HUNGARIAN, Language.ITALIAN);
+        List<Language> languages = filterCriteria.getLanguages();
+        List<Currency> currencies = filterCriteria.getCurrency();
+        Long min = filterCriteria.getPopulation().get("min");
+        Long max = filterCriteria.getPopulation().get("max");
+        return countryRepository.filter(min,max,languages,currencies);
+        //return countryRepository.filter(filterCriteria);
     }
 /*
     @GetMapping("/country/{countryCode}")
