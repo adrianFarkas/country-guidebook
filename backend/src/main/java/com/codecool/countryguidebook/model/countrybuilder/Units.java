6b38938d1339b4773ed76c60cc0bef2b57877502
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-02 09:35:27 +0200

    change currency list type to Currency

--- backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Units.java
+++ backend/src/main/java/com/codecool/countryguidebook/model/countrybuilder/Units.java
@@ -1,6 +1,7 @@
 package com.codecool.countryguidebook.model.countrybuilder;
 
 import com.codecool.countryguidebook.model.Country;
+import com.codecool.countryguidebook.model.Currency;
 import com.codecool.countryguidebook.model.Language;
 import lombok.*;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,7 @@ public class Units {
     private Long id;
     @Singular
     @ElementCollection
-    private List<String> currencies;
+    private List<Currency> currencies;
     @Singular
     @ElementCollection
     private List<Language> languages;
