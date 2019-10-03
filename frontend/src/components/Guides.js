e808cc7b4f18103633d81892660083f521739af4
BihackerViktor <bihacker.viktor@gmail.com>
2019-10-03 12:50:40 +0200

    education

--- frontend/src/components/Guides.js
+++ frontend/src/components/Guides.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Finance from "./Finance";
+import Education from "./Education";
 
 function Guides(props) {
     return (
         <div className="guides">
             <Finance/>
+            <Education/>
         </div>
     );
 }
