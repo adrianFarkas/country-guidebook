841f84721c02c56124ae3c25539f6be962272955
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-03 09:37:01 +0200

    add new option

--- frontend/src/components/InfoLinks.js
+++ frontend/src/components/InfoLinks.js
@@ -5,8 +5,9 @@ function InfoLinks(props) {
     return (
         <div className="info-links">
             <div className="cards">
-                <LinkCard img="/static/img/education.png" title="Education" />
                 <LinkCard img="/static/img/geographic.png" title="Geographic" />
+                <LinkCard img="/static/img/education.png" title="Education" />
+                <LinkCard img="/static/img/sights.png" title="Sights" />
                 <LinkCard img="/static/img/health.png" title="Health Care" />
                 <LinkCard img="/static/img/finance.png" title="Finance" />
             </div>
