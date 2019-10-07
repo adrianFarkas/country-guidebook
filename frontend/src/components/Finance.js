07be867c23bbf65c3146d868ae138f4a13fdf488
BihackerViktor <bihacker.viktor@gmail.com>
2019-10-07 11:07:23 +0200

    iframe content for hungary

--- frontend/src/components/Finance.js
+++ frontend/src/components/Finance.js
@@ -1,42 +1,18 @@
 import React from 'react';
 import Iframe from "react-iframe";
-import {faAlignCenter} from "@fortawesome/free-solid-svg-icons";
 
-function Finance(props) {
-    const {description, stateDebtMillionEuro, averageSalaryEUR, minimumWageEUR} = props.finance;
-
+function Finance() {
     return (
-        <div align={"center"}/*className="detail-card" id="Finance"*/>
-
-            {/*<h1>Finance</h1>*/}
-            {/*<hr/>*/}
-            {/*<div className="detail-card-left-div">*/}
-                <Iframe
-                    url="https://en.wikipedia.org/w/index.php?title=Economy_of_Hungary&printable=yes&fbclid=IwAR3sV-WlGUrv9zIz0qYObhIA6aWjlkrAnK86o-YSJBezDIR1-dYjbL_gSCA"
-                    width="1000px"
-                    height="1000px"
-                    id="Finance"
-                    className="detail-card"
-                    display="initial"
-                    position="relative"/>
-                {/*<div dangerouslySetInnerHTML={{ __html: description }}></div>*/}
-            </div>
-            // <div className="detail-card-right-div">
-            //     <h4>State debt in million Euro</h4>
-            //     <img
-            //         src={stateDebtMillionEuro}
-            //         className="details-card-right-images" alt=""/>
-            //     <h4>Average Salary in {props.currency}</h4>
-            //     <img
-            //         src={averageSalaryEUR}
-            //         className="details-card-right-images" alt=""/>
-            //     <h4>Minimum Wage in Euro</h4>
-            //     <img
-            //         src={minimumWageEUR}
-            //         className="details-card-right-images" alt=""/>
-            //
-            // </div>
-        // </div>
+        <div align={"center"}>
+            <Iframe
+                url="https://en.wikipedia.org/w/index.php?title=Economy_of_Hungary&printable=yes&fbclid=IwAR3sV-WlGUrv9zIz0qYObhIA6aWjlkrAnK86o-YSJBezDIR1-dYjbL_gSCA"
+                width="1000px"
+                height="1000px"
+                id="Finance"
+                className="detail-card"
+                display="initial"
+                position="relative"/>
+        </div>
     );
 }
 
--- frontend/src/components/Geographic.js
+++ frontend/src/components/Geographic.js
@@ -1,38 +1,18 @@
 import React from 'react';
+import Iframe from "react-iframe";
 
-function Geographic(props) {
-    const {description, capital, population, area, timeZones, callingCodes, mapLink} = props.geographic;
+function Geographic() {
     return (
-        <div className="detail-card" id="Geographic">
-
-            <h1>Geographic</h1>
-            <hr/>
-            <div className="detail-card-left-div">
-                <div dangerouslySetInnerHTML={{ __html: description }}></div>
-
-            </div>
-            <div className="detail-card-right-div">
-                <h4>Geography of {props.country}</h4>
-                <img
-                    src={mapLink}
-                    className="details-card-right-images"/>
-
-                <h4>Capital</h4>
-                {capital}
-
-                <h4>Population:</h4>
-                {population}
-
-                <h4>Area:</h4>
-                {area} m<sup>2</sup>
-
-                <h4>Timezone</h4>
-                {timeZones[0]}
-
-                <h4>Calling Code</h4>
-                {callingCodes[0]}
-
-
+        <div>
+            <div align={"center"}>
+                <Iframe
+                    url="https://en.wikipedia.org/w/index.php?title=Education_in_Hungary&printable=yes&fbclid=IwAR3sV-WlGUrv9zIz0qYObhIA6aWjlkrAnK86o-YSJBezDIR1-dYjbL_gSCA"
+                    width="1000px"
+                    height="1000px"
+                    id="Education"
+                    className="detail-card"
+                    display="initial"
+                    position="relative"/>
             </div>
         </div>
     );
