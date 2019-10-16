97a853b62fd9093f3ee2340cf914c32c614df450
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-16 17:39:46 +0200

    remove content from Finance, Education, Sights, Health components for the new design

--- frontend/src/components/Finance.js
+++ frontend/src/components/Finance.js
@@ -1,32 +1,12 @@
 import React from 'react';
+import Guide from "./Guide";
 
 function Finance(props) {
-    const {description, stateDebtMillionEuro, averageSalaryEUR, minimumWageEUR} = props.finance;
 
     return (
-        <div className="detail-card" id="Finance">
+        <Guide title="Finance" id="Finance" >
 
-            <h1>Finance</h1>
-            <hr/>
-            <div className="detail-card-left-div">
-                <div dangerouslySetInnerHTML={{ __html: description }}></div>
-            </div>
-            <div className="detail-card-right-div">
-                <h4>State debt in million Euro</h4>
-                <img
-                    src={stateDebtMillionEuro}
-                    className="details-card-right-images" alt=""/>
-                <h4>Average Salary in {props.currency}</h4>
-                <img
-                    src={averageSalaryEUR}
-                    className="details-card-right-images" alt=""/>
-                <h4>Minimum Wage in Euro</h4>
-                <img
-                    src={minimumWageEUR}
-                    className="details-card-right-images" alt=""/>
-
-            </div>
-        </div>
+        </Guide>
     );
 }
 
--- frontend/src/components/Health.js
+++ frontend/src/components/Health.js
@@ -1,33 +1,11 @@
 import React from 'react';
+import Guide from "./Guide";
 
 function Health(props) {
-    const {description, stateHospital, thermalSpa, hospitalMap} = props.health;
     return (
-        <div className="detail-card" id="Health Care">
+        <Guide title="Health Care" id="Health Care" >
 
-            <h1>Health</h1>
-            <hr/>
-            <div className="detail-card-left-div">
-                <div dangerouslySetInnerHTML={{ __html: description }}></div>
-            </div>
-            <div className="detail-card-right-div">
-                <h4>State Hospital</h4>
-                <img
-                    src={stateHospital}
-                    className="details-card-right-images"/>
-
-                <h4>Thermal Spa in {props.country}</h4>
-                <img
-                    src={thermalSpa}
-                    className="details-card-right-images"/>
-                <h4>Main hospitals in {props.country}</h4>
-                <img
-                    src={hospitalMap}
-                    className="details-card-right-images"/>
-
-
-            </div>
-        </div>
+        </Guide>
     );
 }
 
