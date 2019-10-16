5e535d601a486afa6784e2eb8ae328308c097a00
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-16 17:41:55 +0200

    improve InfoLinks, LinkCard design

--- frontend/src/components/InfoLinks.js
+++ frontend/src/components/InfoLinks.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import LinkCard from "./LinkCard";
+import {
+    faAtlas,
+    faBinoculars,
+    faGlobe,
+    faHandHoldingUsd,
+    faHeartbeat,
+    faUserGraduate
+} from "@fortawesome/free-solid-svg-icons";
 
 function InfoLinks(props) {
     return (
         <div className="info-links">
             <div className="cards">
-                <LinkCard img="/static/img/geographic.png" title="Geographic" />
-                <LinkCard img="/static/img/education.png" title="Education" />
-                <LinkCard img="/static/img/sights.png" title="Sights" />
-                <LinkCard img="/static/img/health.png" title="Health Care" />
-                <LinkCard img="/static/img/finance.png" title="Finance" />
+                <LinkCard icon={faGlobe} title="Geographic" />
+                <LinkCard icon={faUserGraduate} title="Education" />
+                <LinkCard icon={faBinoculars} title="Sights" />
+                <LinkCard icon={faHeartbeat} title="Health Care" />
+                <LinkCard icon={faHandHoldingUsd} title="Finance" />
             </div>
         </div>
     );
