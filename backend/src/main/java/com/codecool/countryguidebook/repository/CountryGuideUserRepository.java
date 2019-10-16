89d0f78e98b173800cb7145531f360c1950c71c0
GaborPap <ppgabor@gmail.com>
2019-10-16 09:40:34 +0200

    Added query for get user from database.

--- backend/src/main/java/com/codecool/countryguidebook/repository/CountryGuideUserRepository.java
+++ backend/src/main/java/com/codecool/countryguidebook/repository/CountryGuideUserRepository.java
@@ -1,7 +1,11 @@
 package com.codecool.countryguidebook.repository;
 
-import com.codecool.countryguidebook.model.CountryGuideUsers;
+import com.codecool.countryguidebook.model.CountryGuideUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryGuideUserRepository extends JpaRepository<CountryGuideUsers, Long> {
+import java.util.Optional;
+
+public interface CountryGuideUserRepository extends JpaRepository<CountryGuideUser, Long> {
+
+    Optional<CountryGuideUser> findByUserName(String username);
 }
