01920d477d895dce6c4da7dadec79338400a2b8b
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-16 13:41:53 +0200

    fix

--- backend/src/main/java/com/codecool/countryguidebook/CountryGuidebookApplication.java
+++ backend/src/main/java/com/codecool/countryguidebook/CountryGuidebookApplication.java
@@ -1,26 +1,14 @@
 package com.codecool.countryguidebook;
 
-import com.codecool.countryguidebook.model.Country;
-import com.codecool.countryguidebook.repository.CountryRepository;
-import org.json.JSONException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.List;
 
 @SpringBootApplication
 public class CountryGuidebookApplication {
 
 
-    @Autowired
-    private CountryRepository countryRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(CountryGuidebookApplication.class, args);
     }
-
 
 }
