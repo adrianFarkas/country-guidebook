147842617cb4c6fdd41391197399ce78929f4026
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-16 10:40:41 +0200

    bug fix

--- backend/src/main/resources/static/sql/health.sql
+++ backend/src/main/resources/static/sql/health.sql
@@ -6,5 +6,5 @@ INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id)
 INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (142,55,32,43,22,NULL,133);
 INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (164,73,55,78,64,NULL,155);
 INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (197,67,78,85,64,NULL,188);
-INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (285,83,68,87,75,NULL,27);
+INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (285,83,68,87,75,NULL,276);
 INSERT INTO health(id,skill,speed,equipment,satisfaction,description,country_id) VALUES (307,74,61,83,51,NULL,298);
