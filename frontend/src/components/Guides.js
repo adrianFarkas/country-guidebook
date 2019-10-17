f06e935e2d26b61f9527871cbbf0a547b8ad52e3
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-17 13:43:47 +0200

    add finance property

--- frontend/src/components/Guides.js
+++ frontend/src/components/Guides.js
@@ -3,18 +3,18 @@ import Geographic from "./Geographic";
 import Sights from "./Sights";
 import Education from "./Education";
 import Health from "./Health";
-import Finance from "./Finance";
+import Finance from "./finance/Finance";
 
 function Guides(props) {
 
-    const {geographic, sights, education, health} = props.country;
+    const {geographic, sights, education, health, finance} = props.country;
 
     return (
         <div className="guides">
             <Geographic country={props.country.name} geographic={geographic}/>
             <Education education={education} />
             <Health health={health} />
-            <Finance />
+            <Finance finance={finance} />
             <Sights sights={sights} />
         </div>
     );
