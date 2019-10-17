0b6c24cf87f0153ac38c2a44c229e15b632644dc
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-17 10:18:11 +0200

    added content for Guides component

--- frontend/src/components/Guides.js
+++ frontend/src/components/Guides.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Geographic from "./Geographic";
+import Sights from "./Sights";
+import Education from "./Education";
+import Health from "./Health";
+import Finance from "./Finance";
 
 function Guides(props) {
 
-    const {geographic} = props.country;
+    const {geographic, sights, education, health} = props.country;
 
     return (
         <div className="guides">
             <Geographic country={props.country.name} geographic={geographic}/>
+            <Education education={education} />
+            <Health health={health} />
+            <Finance />
+            <Sights sights={sights} />
         </div>
     );
 }
