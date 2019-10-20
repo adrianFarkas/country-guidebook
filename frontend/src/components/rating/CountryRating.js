3788aa58bbda9aeb649f5455ae36488ef4e497b1
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-20 15:50:29 +0200

    modified to handle data from server

--- frontend/src/components/rating/CountryRating.js
+++ frontend/src/components/rating/CountryRating.js
@@ -5,6 +5,8 @@ function CountryRating(props) {
     const [hover, setHover] = useState(0);
     const [value, setValue] = useState(0);
 
+    const {rates, sendRate} = props;
+
     const rateBoxStyle = {
         width: "280px",
         height: "280px",
@@ -17,18 +19,30 @@ function CountryRating(props) {
     };
 
 
+    const getNumberOfRatesByValue = (number) => {
+        return rates.filter(r => r.rate === number).length;
+    };
+
+    const getAverageRate = () => {
+        if(rates.length === 0) return (0).toFixed(1);
+        const ratesValues = rates.map(r => r.rate);
+        let sum = ratesValues.reduce((a, b) => a + b, 0);
+        return (sum / rates.length).toFixed(1);
+    };
+
     const handleChange = (event, newHover) => {
         setHover(newHover)
     };
 
     const handleClick = () => {
         setValue(hover);
+        sendRate({rate: hover});
     };
 
     return (
         <div style={rateBoxStyle}>
             <div style={{fontSize: "20px", fontWeight: "bold"}} className="rate-title">Rate this Country</div>
-            <div style={{fontSize: "40px", fontWeight: "bold"}}>3.8</div>
+            <div style={{fontSize: "40px", fontWeight: "bold"}}>{getAverageRate()}</div>
             <div style={{width: "60%", margin: "5px auto 0"}}>
                 <Rate
                     value={value}
@@ -36,11 +50,11 @@ function CountryRating(props) {
                     handleClick={handleClick}
                     hover={hover}
                 />
-                <Rate value={5} disabled={true} title={"(62)"}/>
-                <Rate value={4} disabled={true} title={"(662)"}/>
-                <Rate value={3} disabled={true} title={"(236)"}/>
-                <Rate value={2} disabled={true} title={"(6)"}/>
-                <Rate value={1} disabled={true} title={"(10)"}/>
+                <Rate value={5} disabled={true} title={`(${getNumberOfRatesByValue(5)})`}/>
+                <Rate value={4} disabled={true} title={`(${getNumberOfRatesByValue(4)})`}/>
+                <Rate value={3} disabled={true} title={`(${getNumberOfRatesByValue(3)})`}/>
+                <Rate value={2} disabled={true} title={`(${getNumberOfRatesByValue(2)})`}/>
+                <Rate value={1} disabled={true} title={`(${getNumberOfRatesByValue(1)})`}/>
             </div>
         </div>
     );
